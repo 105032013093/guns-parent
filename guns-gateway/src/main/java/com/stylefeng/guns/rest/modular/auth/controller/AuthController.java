43a95aeebb3ff84a6663d1ad806bcb9b905b664a
105032013093 <773787030@qq.com>
2019-12-14 17:34:19 +0800

    更改用户登录验证逻辑

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.stylefeng.guns.rest.modular.auth.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.UserAPI;
 import com.stylefeng.guns.core.exception.GunsException;
 import com.stylefeng.guns.rest.common.exception.BizExceptionEnum;
 import com.stylefeng.guns.rest.modular.auth.controller.dto.AuthRequest;
 import com.stylefeng.guns.rest.modular.auth.controller.dto.AuthResponse;
 import com.stylefeng.guns.rest.modular.auth.util.JwtTokenUtil;
 import com.stylefeng.guns.rest.modular.auth.validator.IReqValidator;
+import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,20 +28,32 @@ public class AuthController {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
-    @Resource(name = "simpleValidator")
-    private IReqValidator reqValidator;
+    @Reference(interfaceClass = UserAPI.class)
+    private UserAPI userAPI;
 
     @RequestMapping(value = "${jwt.auth-path}")
-    public ResponseEntity<?> createAuthenticationToken(AuthRequest authRequest) {
+    public ResponseVO createAuthenticationToken(AuthRequest authRequest) {
 
-        boolean validate = reqValidator.validate(authRequest);
+        boolean validate = true;
+        // 修改guns内置的用户名密码登录验证机制，自己实现
+        int userId = userAPI.login(authRequest.getUserName(), authRequest.getPassword());
+
+        if(userId==0){
+            validate = false;
+        }
 
         if (validate) {
+            // 生成randomKey和token
             final String randomKey = jwtTokenUtil.getRandomKey();
-            final String token = jwtTokenUtil.generateToken(authRequest.getUserName(), randomKey);
-            return ResponseEntity.ok(new AuthResponse(token, randomKey));
+            final String token = jwtTokenUtil.generateToken("" + userId , randomKey);
+            // 返回值
+
+//            return ResponseEntity.ok(new AuthResponse(token, randomKey));
+            return ResponseVO.succees(new AuthResponse(token, randomKey));
+
         } else {
-            throw new GunsException(BizExceptionEnum.AUTH_REQUEST_ERROR);
+//            throw new GunsException(BizExceptionEnum.AUTH_REQUEST_ERROR);
+            return ResponseVO.serviceFail("用户名或密码错误！");
         }
     }
 }
