0928f8fb2d097c5738da4ef7121a71861a329018
105032013093 <773787030@qq.com>
2019-12-17 20:09:22 +0800

    影片模块

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 演员表 Mapper 接口
+ * </p>
+ *
+ * @author dengsijie
+ * @since 2019-12-16
+ */
+public interface MoocActorTMapper extends BaseMapper<MoocActorT> {
+
+}
