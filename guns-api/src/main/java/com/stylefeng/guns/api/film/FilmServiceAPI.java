359eab131b51a5cbac6a92225ceb6dc48c8af9bf
105032013093 <773787030@qq.com>
2019-12-18 20:09:52 +0800

    重构影片获取API，支持首页/列表模式获取

--- guns-api/src/main/java/com/stylefeng/guns/api/film/FilmServiceAPI.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/film/FilmServiceAPI.java
@@ -10,19 +10,22 @@ public interface FilmServiceAPI {
     List<BannerVO> getBanners();
 
     // 获取热映影片
-    FilmVO getHotFilms(boolean isLimit, int nums);
+    FilmVO getHotFilms(boolean isLimit, int nums, int nowPage, int sortId, int catId, int sourceId, int yearId);
 
     // 获取即将上映影片[受欢迎影片排序]
-    FilmVO getSoonFilms(boolean isLimit,int nums);
+    FilmVO getSoonFilms(boolean isLimit, int nums, int nowPage, int sortId, int catId, int sourceId, int yearId);
+
+    // 获取经典影片[受欢迎影片排序]
+    FilmVO getClassicFilms(int nums, int nowPage, int sortId, int catId, int sourceId, int yearId);
 
     // 获取票房排行榜
     List<FilmInfo> getBoxRanking();
 
     // 获取人气排行榜
-    List<FilmInfo> getExpectRanking ();
+    List<FilmInfo> getExpectRanking();
 
     // 获取Top100
-    List<FilmInfo> getTop ();
+    List<FilmInfo> getTop();
 
     // 获取影片条件接口
     List<CatVO> getCats();
