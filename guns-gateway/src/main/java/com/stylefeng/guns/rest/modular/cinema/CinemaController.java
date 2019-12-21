36623ed64ef61d59764294804ea8aaa218de8ef8
105032013093 <773787030@qq.com>
2019-12-21 14:22:59 +0800

    初始化影院模块Controller

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/cinema/CinemaController.java
@@ -0,0 +1,37 @@
+package com.stylefeng.guns.rest.modular.cinema;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.cinema.CinemaServiceAPI;
+import com.stylefeng.guns.api.cinema.vo.CinemaQueryVO;
+import com.stylefeng.guns.rest.modular.vo.ResponseVO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cinema/")
+public class CinemaController {
+
+    @Reference(interfaceClass = CinemaServiceAPI.class, check = false)
+    private CinemaServiceAPI cinemaServiceAPI;
+
+    @RequestMapping(value = "getCinemas")
+    public ResponseVO getCinemas(CinemaQueryVO cinemaQueryVO) {
+        return null;
+    }
+
+    @RequestMapping(value = "getCondition")
+    public ResponseVO getCondition(CinemaQueryVO cinemaQueryVO) {
+        return null;
+    }
+
+    @RequestMapping(value = "getFields")
+    public ResponseVO getFields(Integer cinemaId) {
+        return null;
+    }
+
+    @RequestMapping(value = "getFieldInfo", method = RequestMethod.POST)
+    public ResponseVO getFieldInfo(Integer cinemaId, Integer fieldId) {
+        return null;
+    }
+}
